411fdac7804aec0be446cbc091c42fc90a329399
IsaacWK1935 <728097@student.dusd.net>
2025-05-20 15:27:53 -0700

    Bonus
    why......?

--- Unit5UserInterfaceP8Ricard/Assets/Scripts/GameManager.cs
+++ Unit5UserInterfaceP8Ricard/Assets/Scripts/GameManager.cs
@@ -9,19 +9,19 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+    public GameObject titleScreen;
     public bool isGameActive;
+    private int lives;
     private int score;
     private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        score = 0;
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -47,7 +47,7 @@ public class GameManager : MonoBehaviour
     }
 
     public void GameOver()
-    {
+    {   
         restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
@@ -56,5 +56,16 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty) 
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
     }
 }
